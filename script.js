783d029e9360b924bc73cf12579a56ed8d5ec695
Henry <124216714+imHenryMa@users.noreply.github.com>
2023-02-13 23:10:09 -0500

    Setup initial structure and completed basic requirements for the project.
    TODO:
    - Confirm password is same as initial password entry
    - Hints for required input boxes if user enters it wrong
    - Validate outlines red after submitting and not only during selection
    - Design for responsive screens ->> Multiple different screen sizes and @media query

---
+++ script.js
@@ -0,0 +1,4 @@
+//TODO: Password input field checks to see if the confirmation is the same as the initial
+//How to do it: on change of the "confirm" password box, have event listener to compare
+//The value of it with the value of what is in the initial passowrd input box
+//If there is a problem, then set the state to invalid
